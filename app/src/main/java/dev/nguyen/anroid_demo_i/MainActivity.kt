9412dcd03ca686d47f23c8c12a5d69f0693cb3fb
Logan Nguyen <nam.p.nguyen131@gmail.com>
2022-05-30 14:00:48 -0500

    Initial commit

---
+++ app/src/main/java/dev/nguyen/anroid_demo_i/MainActivity.kt
@@ -0,0 +1,11 @@
+package dev.nguyen.anroid_demo_i
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
